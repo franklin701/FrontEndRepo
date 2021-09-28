77c390cf0325a2a060e60d2bbcc16da62b2be36a
franklin701 <frnkescobar@gmail.com>
2021-09-27 20:07:52 -0600

    my  login form
    my login from

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,32 +1,58 @@
+import { useState, useRef, useContext } from 'react';
 import { Card, Form, Button, Alert} from 'react-bootstrap';
+import AuthContext from '../../context/auth-context';
+import { useHistory } from 'react-router';
 
 const Login = () => {
+  const authCtx = useContext(AuthContext);
+  const [error, setError] =  useState('');
+  const [disabledSubmit, setDisableSubmit] = useState(false);
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const history = useHistory();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    try {
+      setError('');
+      setDisableSubmit(true);
+      await authCtx.login(emailRef.current.value, passwordRef.current.value);
+      history.push('/');
+    } catch {
+      setError('Fail to create account');
+    }
+    setDisableSubmit(false);
+  }
+  
   return (
-  <div className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+    <div className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
       <div className="w-100" style={{maxWidth: "400px"}}>
         <Card>
           <Card.Body>
-            <h2 className="text-center mb-4">Login</h2>
-        
-            <Form >
+            <h2 className="text-center mb-4">Register</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form onSubmit={handleSubmit}>
               <Form.Group id="email">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email"  required/>
+                <Form.Control type="email" ref={emailRef} required/>
               </Form.Group>
               <Form.Group id="password">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" required/>
+                <Form.Control type="password" ref={passwordRef} required/>
               </Form.Group>
+            
               <Button 
-               
+                disabled={disabledSubmit}
                 className="w-100 mt-3" 
-                type="submit">Login</Button>
+                type="submit">LogIn</Button>
             </Form>
           </Card.Body>
         </Card>
       </div>
     </div>
-    );
+  );
 }
 
 export default Login;
